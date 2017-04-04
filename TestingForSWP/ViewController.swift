6503552181f5bebf626e90b63a2b4da801704758
student <student@students-mbp.fritz.box>
2017-04-04 20:42:44 +0200

    first commit message

--- TestingForSWP/ViewController.swift
+++ TestingForSWP/ViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
+        print("Bla")
         // Do any additional setup after loading the view, typically from a nib.
     }
 

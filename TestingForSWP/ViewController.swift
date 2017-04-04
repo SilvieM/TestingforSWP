e4f6488906715da30bf2a02eece030a5072432a8
student <student@students-mbp.fritz.box>
2017-04-04 20:56:49 +0200

    blank

--- TestingForSWP/ViewController.swift
+++ TestingForSWP/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Bla")
         print("Blabla")
+        
         print("def")
         // Do any additional setup after loading the view, typically from a nib.
     }

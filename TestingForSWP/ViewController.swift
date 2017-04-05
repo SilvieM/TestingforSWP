d70dbf290c7423f4623db72eaef0f747a42631bd
student <student@students-mbp.fritz.box>
2017-04-05 20:15:00 +0200

    removed def

--- TestingForSWP/ViewController.swift
+++ TestingForSWP/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        print("def")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
